b50d576ea5f373af3e0df92835db15d2d7403781
Ayush <ayush@trinitymascot.com>
2019-08-07 20:01:24 +0530

    web

---
+++ includes/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Website</title>
+  <script src="js/vue.js"></script>
+  <script src="js/vue-router.js"></script>
+
+</head>
+
+<!-----------    should be change h1 tags-->
+
+
+<body>
+  <header class="position-relative">
+    <h1>Header</h1>
+  </header>
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+include 'includes/header.php';
+?>
+
+<main role="main">
+	<div>
+		<h4>Hello World</h4>
+	</div>
+</main>
+
+
+<?php include 'includes/footer.php'; ?>
